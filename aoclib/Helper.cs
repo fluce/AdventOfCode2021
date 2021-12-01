3a7cb316282fd6cc1a4ce23fb556311d0d8f9d18
Frédéric Luce <frederic.luce@gmail.com>
2021-12-01 22:59:20 +0100

    Day01

---
+++ aoclib/Helper.cs
@@ -0,0 +1,60 @@
+﻿namespace aoclib;
+public static class Helper
+{
+    static HttpClient httpClient=GetHttpClient();
+
+    static HttpClient GetHttpClient() {
+        var cli=new HttpClient();
+        cli.BaseAddress=new Uri("https://adventofcode.com/");
+        cli.DefaultRequestHeaders.Add("Cookie",$"session={Environment.GetEnvironmentVariable("AOCSESSION")}");
+        return cli;
+    }
+
+    public static async Task<IEnumerable<string>> GetInput(int day) 
+    {
+        Stream stream;
+        var filename=$".input-{day}";
+        var cached=false;
+        var a=Environment.GetCommandLineArgs();
+        if (a.Length>1) {
+            filename=a[1];
+            cached=true;
+        } else {
+            filename=$".input-{day}";
+            cached=File.Exists(filename);
+        }
+        if (cached) 
+            stream=File.OpenRead(filename);
+        else {
+            var res=await httpClient.GetAsync($"2021/day/{day}/input");
+            stream=await res.Content.ReadAsStreamAsync();
+        }
+        using var reader=new StreamReader(stream);
+        var list=new List<string>();
+        while (!reader.EndOfStream)
+            list.Add((await reader.ReadLineAsync())!);
+        
+        if (!cached)
+            await File.WriteAllLinesAsync(filename,list);
+        return list;
+    }
+
+    public static void Deconstruct<T>(this IEnumerable<T> seq, out T first, out IEnumerable<T> rest)
+    {
+        first = seq.First();
+        rest = seq.Skip(1);
+    }
+
+    public static void Deconstruct<T>(this IEnumerable<T> seq, out T first, out T second, out IEnumerable<T> rest)
+        => (first, (second, rest)) = seq;
+
+    public static void Deconstruct<T>(this IEnumerable<T> seq, out T first, out T second, out T third, out IEnumerable<T> rest)
+        => (first, second, (third, rest)) = seq;
+
+    public static void Deconstruct<T>(this IEnumerable<T> seq, out T first, out T second, out T third, out T fourth, out IEnumerable<T> rest)
+        => (first, second, third, (fourth, rest)) = seq;
+
+    public static void Deconstruct<T>(this IEnumerable<T> seq, out T first, out T second, out T third, out T fourth, out T fifth, out IEnumerable<T> rest)
+        => (first, second, third, fourth, (fifth, rest)) = seq;
+
+}
---
+++ day01/Program.cs
@@ -0,0 +1,28 @@
+﻿var input=(await Helper.GetInput(1)).Select(x=>int.Parse(x)).ToArray();
+
+var counter=0;
+var p=0;
+foreach(var (x,i) in input.Select((x,i)=>(x,i)))
+{
+    if (i>0 && x>p) counter++;
+    p=x;
+}
+
+Console.WriteLine($"counter={counter}");
+
+
+int prevsum=0;
+counter=0;
+var (remove,(a1,a2,a3,_))=(0,input);
+var sum=prevsum=a1+a2+a3;
+foreach(var i in input[3..])
+{
+    (remove,a1,a2,a3)=(a1,a2,a3,i);
+    sum+=i-remove;
+    Console.WriteLine($"last: {(a1,a2,a3)} => sum={sum}");
+    if (sum>prevsum) counter++;
+    prevsum=sum;
+}
+
+Console.WriteLine($"counter={counter}");
+
